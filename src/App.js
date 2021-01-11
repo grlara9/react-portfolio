f4f5b64468294efb9b193d1e0a84c0c2b3aa6baf
Guillermo Lara <grlara9@gmail.com>
2021-01-10 18:21:12 -0800

    modified styles on nav links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import Navigation from './components/Navigation'
+import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+      <Switch>
+      <Route>
       <Navigation />
+      </Route>
+      </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 import Header from './Header'
 import './Navigation.css'
@@ -7,7 +8,27 @@ const Navigation =( ) => {
     return (
         <Header >
         <h1 className="navigation__title">Guillermo Lara</h1>
+
+        <nav className="navigation__header-nav">
+        <ul className="nav-links">
+        <li>
+            <NavLink to="">CONTACT</NavLink>
+            </li>
+        
+        <li>
+            <NavLink to="">ABOUT ME</NavLink>
+            </li>
+        
+        <li>
+            <NavLink to="/auth">PORTFOLIO</NavLink>
+            </li>
+    
+      
+    </ul>
+        </nav>
+
         </Header>
+
     )
 }
 export default Navigation;
