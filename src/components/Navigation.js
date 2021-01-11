518ce14311438b9b00c8cb2422b2a5fcf35a0b50
Guillermo Lara <grlara9@gmail.com>
2021-01-10 17:33:58 -0800

    create navigation component all links and logo will be created here

---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Header from './Header'
+import './Navigation.css'
+
+const Navigation =( ) => {
+    return (
+        <Header >
+        <h1 className="navigation__title">Guillermo Lara</h1>
+        </Header>
+    )
+}
+export default Navigation;
