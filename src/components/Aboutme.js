619385e629cbd83e0c379ec392c2fcaaef7aa40c
Guillermo Lara <grlara9@gmail.com>
2021-01-11 21:35:21 -0800

    done with the body of the about me now going to apply styles

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -1,10 +1,26 @@
 import React from 'react'
 
 import './Aboutme.css'
+import guillermo from '../images/guillermo.jpg'
 
 const Aboutme =()=>{
     return(
-        <h1>About me page</h1>
+        <div className="place-item">
+            <div className="card">
+                <div className="place-item__content">
+                    <div className="place-item__image"> 
+                        <img src={guillermo} alt="my profile picture" />
+                        <span>Full Stack Developer</span>
+                    </div>
+                    <div className="place-item__info">
+                        <p>Hi, I'm Guillermo, I'm a full stack developer living in San Diego</p>
+                        <p>
+                        I like to learn from my mistakes. Constantly challenges my knowledge to become the more efficient on what I do.
+                        I specialized working on the backend programming. Enough about me, go checkout some of my work. </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 export default Aboutme
