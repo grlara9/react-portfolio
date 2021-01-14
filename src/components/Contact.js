686895f65e4958a361cbefeb687dfae3dcffd18c
Guillermo Lara <grlara9@gmail.com>
2021-01-13 21:44:38 -0800

    finish with the form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,18 @@ import './Contact.css'
 
 const Contact =()=>{
     return(
-        <h1>hola</h1>
-    )
-}
+       <div className="contact">
+            <h1>Contact Me</h1>
+                <div className="contact__container">
+                    <form>
+                        <input type="text" placeholder="Name" />
+                        <input type="text" placeholder="Email" />
+                        <textarea>Questions/ comments</textarea>
+                        <button type="submit">Submit</button>
+
+                    </form>
+                </div>
+            </div>
+        )
+    }
 export default Contact
