90e66c0c29aa230da1ab5a42c61c4499d6840d25
Guillermo Lara <grlara9@gmail.com>
2021-01-14 23:00:03 -0800

    work on some of the style in img in about me page

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -5,20 +5,21 @@ import guillermo from '../images/guillermo.jpg'
 
 const Aboutme =()=>{
     return(
-       
-                <div className="place-item__content">
-                    <div className="place-item__image"> 
-                        <img src={guillermo} alt="my profile picture" />
-                        <span>Full Stack Developer</span>
-                    </div>
-                    <div className="place-item__info">
-                        <p>Hi, I'm Guillermo, I'm a full stack developer living in San Diego</p>
-                        <p>
-                        I like to learn from my mistakes. Constantly challenges my knowledge to become the more efficient on what I do.
-                        I specialized working on the backend programming. Enough about me, go checkout some of my work. </p>
-                    </div>
-                </div>
-           
+       <div className="container">
+           <div className="aboutme">
+            <div className="place-item__image"> 
+                <img src={guillermo} alt="my profile picture" />
+            </div>
+
+            <div className="place-item__info">
+                <h1>Guillermo Lara</h1>
+                <h2>Full Stack Developer/ React Developer</h2>
+                <p>Hi, I'm Guillermo, I'm a full stack developer living in San Diego</p>
+                <p>I like to learn from my mistakes. Constantly challenges my knowledge to become the more efficient on what I do.
+                I specialized working on the backend programming. Enough about me, go checkout some of my work. </p>
+            </div>
+        </div>
+        </div>
     );
 }
 export default Aboutme
