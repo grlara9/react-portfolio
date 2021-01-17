c6abefa6ef997fa96d04841096543535461ec282
Guillermo Lara <grlara9@gmail.com>
2021-01-17 14:18:22 -0800

    modified about me styles and added icons to main page

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import './Aboutme.css'
 import guillermo from '../images/guillermo.jpg'
 
@@ -13,10 +14,12 @@ const Aboutme =()=>{
 
             <div className="aboutme__info">
                 <h1>Guillermo Lara</h1>
-                <h2>Full Stack Developer/ React Developer</h2>
+                <h2>Full Stack Developer/ React Developer</h2> 
                 <p>Hi, I'm Guillermo, I'm a full stack developer living in San Diego</p>
-                <p>I like to learn from my mistakes. Constantly challenges my knowledge to become the more efficient on what I do.
-                I specialized working on the backend programming. Enough about me, go checkout some of my work. </p>
+    
+            </div>
+            <div className="aboutme__links">
+                <p><GitHubIcon /> | <LinkedInIcon /> </p>
             </div>
         </div>
         </div>
