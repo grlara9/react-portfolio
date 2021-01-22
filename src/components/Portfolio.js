a949f02174a2cad58ff0779af93b564c44b56334
Guillermo Lara <grlara9@gmail.com>
2021-01-21 21:19:04 -0800

    started working on button from portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import googlemern from '../images/google-mern.png'
+import click from '../images/click.png'
 import './Portfolio.css'; 
 
 const Portfolio =()=>{
@@ -23,16 +24,20 @@ const Portfolio =()=>{
             </div>
 
             <div className="project">
-                <div className="project__title">
-                    <h2>Google Book Mern</h2>
+            <div className="project__image">
+                    <img src={click} alt="project image" />
+                    <h1>Dragon Ball Z Memory</h1>
                 </div>
                 <div className="project__specification">
-                    <p>Mern React-based Google Books Search app that displays books on user searches. 
-                        Users can save them to review or purchase later.</p>
+                    <p>A memory challenge game built in React JS. Click on a Dragon ball Z character. But don't click on the same character twice or you lose..</p>
                 </div>
                 <div className="project__links">
-                    <button>hello</button>
-                    <button>Push</button>
+                    <a href="https://ancient-caverns-87467.herokuapp.com/">
+                        <button class="demo btn btn-lg btn-outline-secondary ">Demo</button>
+                    </a>
+                    <a href="https://github.com/grlara9/ClickyGame">
+                        <button class="views btn btn-lg btn-outline-secondary">View</button>
+                    </a>
                 </div>
             </div>
 
