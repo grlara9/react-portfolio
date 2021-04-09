51180330af04ef5614d29d0310e06e275594f316
Guillermo Lara <grlara9@gmail.com>
2021-04-08 21:42:22 -0700

    remove demo button  and make image a link

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -9,21 +9,19 @@ import './Portfolio.css';
 const Portfolio =()=>{
     return(
         <div className="portfolio">
-              
             <div className="project">
                 <div className="project__image">
-                    <img src={googlemern} alt="project image" />
+                    <a href="https://googlesearchappp.herokuapp.com/">
+                        <img src={googlemern} alt="project image" />
+                    </a>
                     <h1>Google Search Book App</h1>
                 </div>
-                
                 <div className="project__specification">
                     <p>Mern React-based Google Books Search app that displays books on user searches. 
-                        Users can save them to review or purchase later.</p>
+                       Users can save them to review or purchase later.</p>
                 </div>
                 <div className="project__links">
-                    <a href="https://googlesearchappp.herokuapp.com/">
-                        <button class="demo btn btn-lg btn-outline-secondary ">Demo</button>
-                    </a>
+                   
                     <a href="https://github.com/grlara9/google-book-search-mern">
                         <button class="views btn btn-lg btn-outline-secondary">View</button>
                     </a>
@@ -31,17 +29,18 @@ const Portfolio =()=>{
             </div>
 
             <div className="project">
-            <div className="project__image">
+                <div className="project__image">
+                <a href="https://ancient-caverns-87467.herokuapp.com/">
                     <img src={click} alt="project image" />
+                       
+                    </a>
                     <h1>Dragon Ball Z Memory</h1>
                 </div>
                 <div className="project__specification">
                     <p>A memory challenge game built in React JS. Click on a Dragon ball Z character. But don't click on the same character twice or you lose..</p>
                 </div>
                 <div className="project__links">
-                    <a href="https://ancient-caverns-87467.herokuapp.com/">
-                        <button class="demo btn btn-lg btn-outline-secondary ">Demo</button>
-                    </a>
+                   
                     <a href="https://github.com/grlara9/ClickyGame">
                         <button class="views btn btn-lg btn-outline-secondary">View</button>
                     </a>
@@ -50,16 +49,17 @@ const Portfolio =()=>{
 
             <div className="project">
             <div className="project__image">
-                    <img src={burger} alt="project image" />
+            <a href="https://fast-woodland-65175.herokuapp.com/">
+            <img src={burger} alt="project image" />
+                    </a>
+                    
                     <h1>Eat Your Burger</h1>
                 </div>
                 <div className="project__specification">
                     <p>A burger logger with MySQL, Node, Express, Handlebars and using Sequelize.js. Eat-Da-Burger! is a restaurant app that lets users input the names of burgers they'd like to eat.</p>
                 </div>
                 <div className="project__links">
-                    <a href="https://fast-woodland-65175.herokuapp.com/">
-                        <button class="demo btn btn-lg btn-outline-secondary ">Demo</button>
-                    </a>
+                   
                     <a href="https://github.com/grlara9/sequelizeBurger">
                         <button class="views btn btn-lg btn-outline-secondary">View</button>
                     </a>
@@ -68,16 +68,17 @@ const Portfolio =()=>{
 
             <div className="project">
             <div className="project__image">
+            <a href="https://grlara9.github.io/react-weather-app-hooks/">
                     <img src={weatherapp} alt="project image" />
+                       
+                    </a>
                     <h1>React Weather App</h1>
                 </div>
                 <div className="project__specification">
                     <p>A Weather app built in React js using OpenWeathermap API. User can search by typing city. Default temperature is the users location.</p>
                 </div>
                 <div className="project__links">
-                    <a href="https://grlara9.github.io/react-weather-app-hooks/">
-                        <button class="demo btn btn-lg btn-outline-secondary ">Demo</button>
-                    </a>
+                    
                     <a href="https://github.com/grlara9/react-weather-app-hooks">
                         <button class="views btn btn-lg btn-outline-secondary">View</button>
                     </a>
@@ -87,16 +88,17 @@ const Portfolio =()=>{
 
             <div className="project">
             <div className="project__image">
+            <a href="https://clone-4aa3d.web.app/">
                     <img src={amazon2} alt="project image" />
+                       
+                    </a>
                     <h1>Amazon Clone App</h1>
                 </div>
                 <div className="project__specification">
                     <p>Amazon Clone is a project from cleverprogrammer where you can create a product, add the products to the cart, do a payment, and watch your order history.</p>
                 </div>
                 <div className="project__links">
-                    <a href="https://clone-4aa3d.web.app/">
-                        <button class="demo btn btn-lg btn-outline-secondary ">Demo</button>
-                    </a>
+                   
                     <a href="https://github.com/grlara9/amazon_clone">
                         <button class="views btn btn-lg btn-outline-secondary">View</button>
                     </a>
